61b264a5aa8a89f7800d662a16db1c0713cf95e9
oraa <743770701@qq.com>
2016-12-29 22:57:13 +0800

    firstpublish

---
+++ PBL/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace WindowsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(431, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始游戏";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(431, 32);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(196, 302);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(431, 357);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "显示走棋提示";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(539, 357);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 30);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "清除走棋提示";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 435);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(779, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(131, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 32);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(539, 402);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(93, 30);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "结束游戏";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button4_MouseMove);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(649, 402);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 30);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "结束游戏";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button5_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(633, 88);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(633, 179);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(649, 115);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "确认";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(649, 206);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "确认";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(633, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "黑方：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(633, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "白方：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(627, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 14);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "认爹游戏双方选手";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(631, 245);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "必读说明：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(633, 290);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 12);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "2.认爹结束前无法退出";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(633, 267);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 12);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "1.双方自愿参加游戏";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(633, 312);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 12);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "3.名字一旦确认无法修改";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 457);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "谁是爸爸";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ PBL/Form1.cs
@@ -0,0 +1,445 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private readonly string[] Info = new string[61];
+        private readonly int[,] Map = new int[10,10];
+        //0无子 1黑色 2白色 
+        private int MyColor;
+        private int x1;
+        //落子位置 
+        private int y1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Show_Can_Position()
+        {
+            //用图片形式显示可以落子的位置 
+            int i;
+            int j;
+            Graphics g = pictureBox1.CreateGraphics();
+            Bitmap bitmap = new Bitmap("Info2.png");
+            //提示图片 
+            int n = 0;
+            for (i = 1; i <= 8; i++)
+            {
+                for (j = 1; j <= 8; j++)
+                {
+                    if (Map[i, j] == 0 & Can_go(i, j))
+                    {
+                        Info[n] = i + "|" + j;
+                        n = n + 1;
+                        g.DrawImage(bitmap, (i - 1)*45 + 26, (j - 1)*45 + 26, 30, 30);
+                    }
+                }
+            }
+        }
+
+        //统计可以落子的位置数 
+        private int Show_Can_Num()
+        {
+            int i, j;
+            int n = 0;
+            for (i = 1; i <= 8; i++)
+            {
+                for (j = 1; j <= 8; j++)
+                {
+                    if (Can_go(i, j))
+                    {
+                        Info[n] = i + "|" + j;
+                        n = n + 1;
+                    }
+                }
+            }
+            return n;
+            //可以落子的位置个数 
+        }
+
+        private void Cls_Can_Position()
+        {
+            int n;
+            string a;
+            string b;
+            int x;
+            int y;
+            string s;
+            Graphics g = pictureBox1.CreateGraphics();
+            Bitmap bitmap = new Bitmap("BackColor.png");
+            //背景图片 
+            for (n = 0; n <= 60; n++)
+            {
+                s = Info[n];
+                if (string.IsNullOrEmpty(s)) break;
+
+                a = s.Substring(0, 1);
+                //b = s.Substring(Strings.InStr(s, "|"), 1);
+                b = s.Substring(s.IndexOf('|', 1) + 1);
+                x = Convert.ToInt16(a);
+                y = Convert.ToInt16(b);
+                if (Map[x, y] == 0)
+                {
+                    g.DrawImage(bitmap, (x - 1)*45 + 26, (y - 1)*45 + 26, 30, 30);
+                }
+                //Me.Text = CInt(x) & y 
+            }
+        }
+
+        private bool CheckDirect(int x1, int y1, int dx, int dy)
+        {
+            int x, y;
+            bool flag;
+            x = x1 + dx;
+            y = y1 + dy;
+            flag = false;
+            while (InBoard(x, y) & !Ismychess(x, y) & Map[x, y] != 0)
+            {
+                x += dx;
+                y += dy;
+                flag = true; //构成夹击之势 
+            }
+            if (InBoard(x, y) & Ismychess(x, y) & flag)
+            {
+                return true; //该方向落子有效 
+            }
+            return false;
+        }
+
+        private void DirectReverse(int x1, int y1, int dx, int dy)
+        {
+            int x, y;
+            bool flag;
+            x = x1 + dx;
+            y = y1 + dy;
+            flag = false;
+            while (InBoard(x, y) & !Ismychess(x, y) & Map[x, y] != 0)
+            {
+                x += dx;
+                y += dy;
+                flag = true; //构成夹击之势 
+            }
+            if (InBoard(x, y) & Ismychess(x, y) & flag)
+            {
+                do
+                {
+                    x -= dx;
+                    y -= dy;
+                    if ((x != x1 || y != y1)) FanQi(x, y);
+                } while ((x != x1 || y != y1));
+            }
+        }
+
+        private bool InBoard(int x, int y)
+        {
+            if (x >= 1 & x <= 8 & y >= 1 & y <= 8)
+                return true;
+            else
+                return false;
+        }
+
+        private bool Can_go(int x1, int y1)
+        {
+            //从左，左上，上，右上，右，右下，下，左下八个方向判断
+
+            if (CheckDirect(x1, y1, -1, 0))
+                return true;
+            if (CheckDirect(x1, y1, -1, -1))
+                return true;
+            if (CheckDirect(x1, y1, 0, -1))
+                return true;
+            if (CheckDirect(x1, y1, 1, -1))
+                return true;
+            if (CheckDirect(x1, y1, 1, 0))
+                return true;
+            if (CheckDirect(x1, y1, 1, 1))
+                return true;
+            if (CheckDirect(x1, y1, 0, 1))
+                return true;
+            if (CheckDirect(x1, y1, -1, 1))
+                return true;
+            return false;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int x, y;
+            int n = 0;
+            x1 = (e.X - 22)/45 + 1;
+            y1 = (e.Y - 22)/45 + 1;
+            if (!Can_go(x1, y1))
+            {
+                toolStripStatusLabel1.Text = "此处不能走棋子";
+                return;
+            }
+
+            if (MyColor == 1)
+            {
+                listBox1.Items.Add("黑色落在(" + Convert.ToChar(x1 + 64).ToString() + "，" + y1 + ")");
+            }
+            else
+            {
+                listBox1.Items.Add("白色落在(" + Convert.ToChar(x1 + 64).ToString() + "，" + y1 + ")");
+            }
+
+            //(x1,y1)处原色处理 
+            Graphics g = pictureBox1.CreateGraphics();
+            Bitmap bitmap = new Bitmap("WhiteStone.png");
+            if (MyColor == 2)
+            {
+                Map[x1, y1] = 2;
+                g.DrawImage(bitmap, (x1 - 1)*45 + 22, (y1 - 1)*45 + 22, 45, 45);
+            }
+            if (MyColor == 1)
+            {
+                Map[x1, y1] = 1;
+                bitmap = new Bitmap("BlackStone.png");
+                g.DrawImage(bitmap, (x1 - 1)*45 + 22, (y1 - 1)*45 + 22, 45, 45);
+            }
+
+            //从左，左上，上，右上，右，右下，下，左下八个方向翻转 
+            if (CheckDirect(x1, y1, -1, 0)) //向左方向形成夹击之势
+                DirectReverse(x1, y1, -1, 0);
+            if (CheckDirect(x1, y1, -1, -1)) //向左上方向形成夹击之势
+                DirectReverse(x1, y1, -1, -1);
+            if (CheckDirect(x1, y1, 0, -1)) //向上方向形成夹击之势
+                DirectReverse(x1, y1, 0, -1);
+            if (CheckDirect(x1, y1, 1, -1)) //向右上方向形成夹击之势
+                DirectReverse(x1, y1, 1, -1);
+
+            if (CheckDirect(x1, y1, 1, 0))
+                DirectReverse(x1, y1, 1, 0);
+            if (CheckDirect(x1, y1, 1, 1))
+                DirectReverse(x1, y1, 1, 1);
+            if (CheckDirect(x1, y1, 0, 1))
+                DirectReverse(x1, y1, 0, 1);
+            if (CheckDirect(x1, y1, -1, 1))
+                DirectReverse(x1, y1, -1, 1);
+            Cls_Can_Position(); //清除提示             
+            if (MyColor == 1)
+            {
+                //状态行提示该对方走棋 
+                MyColor = 2;
+                toolStripStatusLabel1.Text = "白色棋子走";
+            }
+            else
+            {
+                MyColor = 1;
+                toolStripStatusLabel1.Text = "黑色棋子走";
+            }
+            Show_Can_Position(); //显示提示             
+            if (Show_Can_Num() == 0)
+            {
+                MessageBox.Show("提示", "对方无可走位置,请继续走棋");
+                if (MyColor == 1)
+                {
+                    MyColor = 2;
+                    toolStripStatusLabel1.Text = "白色棋子继续走";
+                }
+                else
+                {
+                    MyColor = 1;
+                    toolStripStatusLabel1.Text = "黑色棋子继续走";
+                }
+                Show_Can_Position(); //显示提示 
+            }
+            //判断游戏是否结束 
+            int whitenum = 0;
+            int blacknum = 0;
+            for (x = 1; x <= 8; x++)
+            {
+                for (y = 1; y <= 8; y++)
+                {
+                    if (Map[x, y] != 0)
+                    {
+                        n = n + 1;
+                        if (Map[x, y] == 2)
+                            whitenum += 1;
+                        if (Map[x, y] == 1)
+                            blacknum += 1;
+                    }
+                }
+            }
+            if (n == 64) //在棋盘下满时， 
+            {
+                if (blacknum > whitenum)
+                {
+                    MessageBox.Show("游戏结束，恭喜黑方爸爸胜利，白方儿子你的父亲还有刘洋 李文全 宫矫健 姜建鹏 王宏宇。", "黑方:" + blacknum + "白方:" + whitenum);
+                }
+                else
+                {
+                    MessageBox.Show("游戏结束，恭喜白方爸爸胜利，黑方儿子你的父亲还有刘洋 李文全 宫矫健 姜建鹏 王宏宇。", "黑方:" + blacknum + "白方:" + whitenum);
+                }
+                Application.Exit();
+                pictureBox1.Enabled = false; //游戏结束，不能走棋 
+                button1.Enabled = true; //"开始游戏"按钮有效 
+                return;
+            }
+            //在棋盘还没有下满时 
+            if (whitenum == 0)
+            {
+                MessageBox.Show("游戏结束，恭喜黑方爸爸胜利，白方儿子你的父亲还有刘洋 李文全 宫矫健 姜建鹏 王宏宇。", "黑方:" + blacknum + "白方:" + whitenum);
+                pictureBox1.Enabled = false; //游戏结束，不能走棋 
+                button1.Enabled = true; //"开始游戏"按钮有效 
+                Application.Exit();
+            }
+            if (blacknum == 0)
+            {
+                MessageBox.Show("游戏结束，恭喜白方爸爸胜利，黑方儿子你的父亲还有刘洋 李文全 宫矫健 姜建鹏 王宏宇。", "黑方:" + blacknum + "白方:" + whitenum);
+                pictureBox1.Enabled = false; //游戏结束，不能走棋 
+                button1.Enabled = true; //"开始游戏"按钮有效 
+                Application.Exit();
+            }
+        }
+
+
+        private void FanQi(int x, int y)
+        {
+            Graphics g = pictureBox1.CreateGraphics();
+            Bitmap bitmap = new Bitmap("WhiteStone.png");
+
+            //1黑色 2白色 
+            if (Map[x, y] == 1)
+            {
+                Map[x, y] = 2;
+                g.DrawImage(bitmap, (x - 1)*45 + 22, (y - 1)*45 + 22, 45, 45);
+            }
+            else
+            {
+                Map[x, y] = 1;
+                bitmap = new Bitmap("BlackStone.png");
+                g.DrawImage(bitmap, (x - 1)*45 + 22, (y - 1)*45 + 22, 45, 45);
+            }
+            Thread.Sleep(200); //延时0.2秒
+            listBox1.Items.Add(" (" + Convert.ToChar(x + 64).ToString() + "，" + y + ")处被反色");
+        }
+
+        private bool Ismychess(int x, int y)
+        {
+            if (Map[x, y] == MyColor)
+                return true;
+            else
+                return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //开始游戏按钮事件过程 
+            int x, y;
+            Graphics g = pictureBox1.CreateGraphics();
+            Bitmap bitmap = new Bitmap("WhiteStone.png");
+            pictureBox1.Enabled = true;
+            pictureBox1.Refresh();
+            for (x = 1; x <= 8; x++)
+            {
+                for (y = 1; y <= 8; y++)
+                {
+                    Map[x, y] = 0;
+                }
+            }
+            listBox1.Items.Clear();
+            //棋盘上画初始４个棋子 
+            x = 4;
+            y = 4;
+            g.DrawImage(bitmap, (x - 1)*45 + 22, (y - 1)*45 + 22, 45, 45);
+            x = 5;
+            y = 5;
+            g.DrawImage(bitmap, (x - 1)*45 + 22, (y - 1)*45 + 22, 45, 45);
+            bitmap = new Bitmap("BlackStone.png");
+            x = 5;
+            y = 4;
+            g.DrawImage(bitmap, (x - 1)*45 + 22, (y - 1)*45 + 22, 45, 45);
+            x = 4;
+            y = 5;
+            g.DrawImage(bitmap, (x - 1)*45 + 22, (y - 1)*45 + 22, 45, 45);
+            Map[4, 4] = 2;
+            //0无子 1黑色 2白色 
+            Map[5, 5] = 2;
+            Map[4, 5] = 1;
+            Map[5, 4] = 1;
+            MyColor = 1;
+            //自己棋子颜色为黑色 
+            toolStripStatusLabel1.Text = "黑色棋子先走";
+            Text = "黑白棋游戏";
+            Show_Can_Position();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Show_Can_Position();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Cls_Can_Position();
+        }
+
+        private void button4_MouseMove(object sender, MouseEventArgs e)
+        {
+            button5.Visible = true;
+            button4.Visible = false;
+        }
+
+        private void button5_MouseMove(object sender, MouseEventArgs e)
+        {
+            button5.Visible = false;
+            button4.Visible = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            switch (e.CloseReason)
+{
+    //自身窗口上的关闭按钮
+    case CloseReason.FormOwnerClosing:
+            e.Cancel = true;//拦截，不响应操作
+            break;
+    //MDI窗体关闭事件
+    case CloseReason.MdiFormClosing:
+            e.Cancel = true;//拦截，不响应操作
+            break;
+    //不明原因的关闭
+    case CloseReason.None:
+            break;
+    //用户通过UI关闭窗口或者通过Alt+F4关闭窗口
+    case CloseReason.UserClosing:
+            e.Cancel = true;//拦截，不响应操作
+            break;
+    //操作系统准备关机
+    case CloseReason.WindowsShutDown:
+            e.Cancel = false;//不拦截，响应操作
+            break;
+    default:
+            break;
+}
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox2.ReadOnly = true;
+        }
+
+
+
+    }
+}
